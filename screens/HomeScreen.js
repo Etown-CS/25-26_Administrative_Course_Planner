efdbb48a764cd741aa5cb971409a962117f5273a
Sparkle26 <zourgania@etown.edu>
2025-10-14 21:23:26 -0400

    .

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -47,11 +47,11 @@ const styles = StyleSheet.create({
   },
   header: {
     width: '100%',
-    height: 100,
+    height: 90,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#013d95',
-    borderRadius: 15,
+    borderRadius: 1,
     shadowColor: '#000',
   },
   title: {
