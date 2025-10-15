1a065ecd90a5b598d817ba1de458b18f81cf6dc1
Sparkle26 <zourgania@etown.edu>
2025-10-14 21:02:44 -0400

    remove markers

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,157 +1,63 @@
 import React from 'react';
-import { TextInput, View, Text, Alert, StyleSheet } from "react-native";
-//import Dropdown from 'react-bootstrap/Dropdown';
-//import DropdownButton from 'react-bootstrap/DropdownButton';
-//import { Picker } from '@react-native-picker/picker';
-//import { useState } from "react";
-
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
-    return (
+  return (
     <View style={styles.container}>
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    {/* this is where i put the header box */}
-    <View style={styles.header}>
-    </View>
+      {/* Header box */}
+      <View style={styles.header}></View>
 
       <Text style={styles.title}>Make a Schedule</Text>
-      <Text style={styles.subtitle}>Use the drop down menu to select which best works for you to generate.
+      <Text style={styles.subtitle}>
+        Use the drop down menu to select which best works for you to generate.
       </Text>
-    
-<<<<<<< HEAD
-    {/* this is the year box */}
-    <View style={styles.year}>
+
+      {/* Year box */}
+      <View style={styles.year}>
         <Text style={styles.yearTitle}>Year</Text>
+      </View>
 
-    </View>
-
-    {/* this is the major box */}
-    <View style={styles.major}>
+      {/* Major box */}
+      <View style={styles.major}>
         <Text style={styles.majorTitle}>Major</Text>
-    </View>
+      </View>
 
-    {/* this is the concentration box */}
-    <View style={styles.concentration}>
+      {/* Concentration box */}
+      <View style={styles.concentration}>
         <Text style={styles.concentrationTitle}>Concentration</Text>
-    </View>
+      </View>
 
-    {/* this is the bottom circle */}
-    <View style={styles.halfCircle}>
-        <Text style={styles.halfCircle}></Text>
-    </View>
+      {/* Bottom half circle */}
+      <View style={styles.halfCircle}></View>
 
-    {/* this is the generate circle */}
-    <View style={styles.generate}>
+      {/* Generate button */}
+      <View style={styles.generate}>
         <Text style={styles.generateTitle}>GENERATE</Text>
-    </View>
-
-=======
-{/*<this is where i put the header box>*/}
-=======
-    {/* this is where i put the header box */}
->>>>>>> 8b5843b (adding year area)
-    <View style={styles.header}>
-    </View>
-
-      <Text style={styles.title}>Make a Schedule</Text>
-<<<<<<< HEAD
-      <Text style={styles.subtitle}>Use the drop down menu to select which best works for you to generate.</Text>
->>>>>>> 13bebfb (the title and text)
-=======
-      <Text style={styles.subtitle}>Use the drop down menu to select which best works for you to generate.
-      </Text>
-    
-
-=======
->>>>>>> 3cabb8d (added generate button interface)
-    {/* this is the year box */}
-    <View style={styles.year}>
-        <Text style={styles.yearTitle}>Year</Text>
-    </View>
-
-    {/* this is the major box */}
-    <View style={styles.major}>
-        <Text style={styles.majorTitle}>Major</Text>
-    </View>
-
-    {/* this is the concentration box */}
-    <View style={styles.concentration}>
-        <Text style={styles.concentrationTitle}>Concentration</Text>
-    </View>
-
-    {/* this is the bottom circle */}
-    <View style={styles.halfCircle}>
-        <Text style={styles.halfCircle}></Text>
-    </View>
-
-    {/* this is the generate circle */}
-    <View style={styles.generate}>
-        <Text style={styles.generateTitle}>GENERATE</Text>
-    </View>
-
->>>>>>> 8b5843b (adding year area)
+      </View>
     </View>
   );
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 288fd44 (adding blue box to top of page)
-
 const styles = StyleSheet.create({
-
-    header: {
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: '#fff',
+  },
+  header: {
     width: '100%',
     height: 100,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#013d95ff',
-    borderradius: 15,
+    backgroundColor: '#013d95',
+    borderRadius: 15,
     shadowColor: '#000',
-    },
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-const styles = StyleSheet.create({
->>>>>>> 13bebfb (the title and text)
-=======
-
-
->>>>>>> 288fd44 (adding blue box to top of page)
-=======
->>>>>>> 8b5843b (adding year area)
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start', 
-    alignItems: 'flex-start',  
-<<<<<<< HEAD
-<<<<<<< HEAD
-    backgroundColor: 'hsla(0, 0%, 100%, 1.00)',
-
-=======
->>>>>>> 13bebfb (the title and text)
-=======
-    backgroundColor: 'hsla(0, 0%, 100%, 1.00)',
-
->>>>>>> 3cabb8d (added generate button interface)
   },
-
   title: {
     fontSize: 28,
     fontWeight: 'bold',
-<<<<<<< HEAD
-<<<<<<< HEAD
     marginTop: 20,
-=======
-    marginTop: 100,
->>>>>>> 13bebfb (the title and text)
-=======
-    marginTop: 20,
->>>>>>> 8b5843b (adding year area)
     marginLeft: 20,
     color: '#333',
   },
@@ -159,103 +65,78 @@ const styles = StyleSheet.create({
     fontSize: 15,
     width: '80%',
     fontStyle: 'italic',
-    flexWrap: 'wrap',
     color: '#555',
     marginTop: 5,
     marginLeft: 20,
   },
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8b5843b (adding year area)
-
-    year: {
+  year: {
     width: '20%',
     height: 30,
     borderRadius: 20,
     marginTop: 20,
     marginLeft: 20,
-    backgroundColor: '#c70202ff',
-    shadowColor: '#000',
-    },
-    
-    yearTitle: {
+    backgroundColor: '#c70202',
+  },
+  yearTitle: {
     color: '#fff',
     fontWeight: 'bold',
     textAlign: 'center',
     marginTop: 5,
     fontSize: 20,
-    },
-
-    major: {
+  },
+  major: {
     width: '20%',
     height: 30,
     borderRadius: 20,
     marginTop: 20,
     marginLeft: 20,
-    backgroundColor: '#c70202ff',
-    shadowColor: '#000',
-    },
-    
-    majorTitle: {
+    backgroundColor: '#c70202',
+  },
+  majorTitle: {
     color: '#fff',
     fontWeight: 'bold',
     textAlign: 'center',
     marginTop: 5,
     fontSize: 20,
-    },
-
-    concentration: {
+  },
+  concentration: {
     width: '40%',
     height: 30,
     borderRadius: 20,
     marginTop: 20,
     marginLeft: 20,
-    backgroundColor: '#c70202ff',
-    shadowColor: '#000',
-    },
-    
-    concentrationTitle: {
+    backgroundColor: '#c70202',
+  },
+  concentrationTitle: {
     color: '#fff',
     fontWeight: 'bold',
     textAlign: 'center',
     marginTop: 5,
     fontSize: 20,
-    },
-
-    generate: {
+  },
+  generate: {
     width: '40%',
     height: 30,
     borderRadius: 20,
     marginTop: 350,
     marginLeft: 117,
-    backgroundColor: '#031d52ff',
-    shadowColor: '#000',
-    },
-    
-    generateTitle: {
+    backgroundColor: '#031d52',
+  },
+  generateTitle: {
     color: '#fff',
     fontWeight: 'bold',
     textAlign: 'center',
     marginTop: 5,
     fontSize: 20,
-    },
-
-    halfCircle: {
+  },
+  halfCircle: {
     position: 'absolute',
     bottom: 0,
-    left: '0%',
-    width: 390, 
+    left: 0,
+    width: 390,
     height: 100,
     backgroundColor: '#3498db',
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20, 
     borderTopLeftRadius: 100,
     borderTopRightRadius: 100,
-    
-    },
-
-
-
-
+  },
 });
