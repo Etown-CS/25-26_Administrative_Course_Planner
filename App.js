2167f6fa5c0b5a758d069c4bd4a3c4c03bfbbf4b
Sparkle26 <zourgania@etown.edu>
2025-10-17 12:53:05 -0400

    comment out courseplanner screen

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import HomeScreen from './screens/HomeScreen';
 //import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import  LoginScreen  from './screens/LoginScreen';
-import CoursePlannerScreen  from './screens/CoursePlannerScreen';
+//import CoursePlannerScreen  from './screens/CoursePlannerScreen'; removed for now for mine to work
 //import CoursePlannerScreen from './screens/CoursePlannerScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
