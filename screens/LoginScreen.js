c1e232262f71fdf2a75369936cd9a34bd760ebf1
Sparkle26 <zourgania@etown.edu>
2025-10-23 12:55:06 -0400

    adding text to the field

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -17,32 +17,76 @@ function LoginScreen({ navigation }) {
         }
     };
 
-    return (
-        <Login></Login>
-    );
+return (
+  <View style={{flex: 1, backgroundColor: '#fff'}}>
+    <Text style={styles.title}>The Nest Planner!</Text>
+    <Text style={styles.subtitle}>Ready to take Flight!</Text>
+    <Text style={styles.logintext}>Login</Text>
+    <Login />
+  </View>
+);
+
+    
 
 }
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+    background: {
+        backgroundColor: 'white',
+
+    },
     container: {
         flex: 1,
         justifyContent: 'top',
         padding: 20,
     },
     title: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        marginBottom: 20,
+        color: '#004B98',
         textAlign: 'center',
-    },
+        fontFamily: 'Roboto',
+        fontSize: 40,
+        fontStyle: 'normal',
+        fontWeight: '900',
+        lineHeight: 41.5,
+        marginTop: 210,
+        marginBottom: 20,
+  },
+    subtitle: {
+        color: '#3DB5E6',
+        textAlign: 'center',
+        fontFamily: 'Roboto',
+        fontSize: 32,
+        fontStyle: 'normal',
+        fontWeight: '900',
+        lineHeight: 41.5,
+        marginBottom: 20,
+            },
+    logintext: {
+        color: '#0A2240',
+        fontFamily: 'Roboto',
+        fontSize: 29,
+        textAlign: 'left',
+        fontWeight: '800',
+        lineHeight: 32,
+        marginTop: 40,
+        marginBottom: 8,
+        alignSelf: 'flex-start',
+        marginLeft: 30,
+  },
     logininput: {
         height: 40,
         borderColor: 'gray', 
         borderWidth: 1, 
         marginBottom: 10, 
-        paddingLeft: 8
+        paddingLeft: 8,
     }
 
 });
