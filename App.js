977871596fbcf96ab0817742729a05da719bf132
Parker <englep@etown.edu>
2025-11-05 12:08:58 -0500

    Removing unneccesary commented out imports, improved readability

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import HomeScreen from './screens/HomeScreen';
 import { StyleSheet, Text, View } from 'react-native';
-//import CoursePlannerScreen  from './screens/CoursePlannerScreen'; removed for now for mine to work
-//import  LoginScreen  from './screens/LoginScreen';   uncomment when login is finished
 import  LoginScreen  from './screens/LoginScreen';
-//import CoursePlannerScreen from './screens/CoursePlannerScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CalendarScreen from './screens/calendarScreen';
-// import Login from './components/Login';
+
 
 
 
@@ -17,11 +14,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen}
-        options={{headerShown:false}} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown:false}} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="CalendarScreen" component={CalendarScreen} />
-        {/* <Stack.Screen name="LoginScreen" component={LoginScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
